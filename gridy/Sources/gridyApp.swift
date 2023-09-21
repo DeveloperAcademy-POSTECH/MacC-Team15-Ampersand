5fcf639210aaa2f32cad56f376c2707b15660e8d
Dayeon Moon <0217dayun@naver.com>
2023-09-21 22:37:54 +0900

    [docs] gitignore 생성

---
+++ gridy/Sources/gridyApp.swift
@@ -0,0 +1,17 @@
+//
+//  gridyApp.swift
+//  gridy
+//
+//  Created by 제나 on 2023/09/21.
+//
+
+import SwiftUI
+
+@main
+struct gridyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
