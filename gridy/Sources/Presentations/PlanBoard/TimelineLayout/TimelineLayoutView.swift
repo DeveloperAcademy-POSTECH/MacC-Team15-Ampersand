f40fd41ce2ab6131517b56c4ef5959babaaedd8b
GaOn <xnoag@icloud.com>
2023-09-29 13:56:49 +0900

    [feat][#10] RightToolBarArea 배치
    NavigationSplitView로 RightToolBarArea를 배치했을 때 토글되지 않는 점을, .inspector로 재배치 완료

--- gridy/Sources/Presentations/PlanBoard/TimelineLayout/TimelineLayoutView.swift
+++ gridy/Sources/Presentations/PlanBoard/TimelineLayout/TimelineLayoutView.swift
@@ -7,20 +7,31 @@
 
 import SwiftUI
 
+@available(macOS 14.0, *)
 struct TimelineLayoutView: View {
+    @State private var showingRightToolBarArea: Bool = true
+    
     var body: some View {
         NavigationSplitView {
             LeftToolBarAreaView()
                 .navigationSplitViewColumnWidth(min: 240, ideal: 240, max: 480)
-        } content: {
-            TimelineLayoutContentView()
         } detail: {
-            RightToolBarAreaView()
-                .navigationSplitViewColumnWidth(240)
+            TimelineLayoutContentView()
+                .inspector(isPresented: $showingRightToolBarArea) {
+                    RightToolBarAreaView()
+                        .inspectorColumnWidth(240)
+                }
+        }
+        .toolbar {
+            Toggle(isOn: $showingRightToolBarArea) {
+                Image(systemName: "sidebar.trailing")
+            }
+            .toggleStyle(.button)
         }
     }
 }
 
+@available(macOS 14.0, *)
 struct TimelineLayoutView_Previews: PreviewProvider {
     static var previews: some View {
         TimelineLayoutView()
