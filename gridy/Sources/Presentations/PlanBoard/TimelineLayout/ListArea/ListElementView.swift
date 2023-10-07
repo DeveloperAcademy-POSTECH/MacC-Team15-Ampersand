8310a394860f54e3a015e6e4e1cb1765041b7e0c
GaOn <xnoag@icloud.com>
2023-10-07 18:04:29 +0900

    [feat][#19] 텍스트 입력 되어있을 때 왼쪽 오른쪽 추가 박스 호버링 개발

--- gridy/Sources/Presentations/PlanBoard/TimelineLayout/ListArea/ListElementView.swift
+++ gridy/Sources/Presentations/PlanBoard/TimelineLayout/ListArea/ListElementView.swift
@@ -12,6 +12,8 @@ struct ListElementView: View {
     @State private var isEditing = false
     @State private var listElementText = ""
     @State private var isEnterShorCut = false
+    @State private var isLeftButtonOpening = false
+    @State private var isRightButtonOpening = false
     @FocusState private var isTextFieldFocused: Bool
     
     var body: some View {
@@ -27,7 +29,7 @@ struct ListElementView: View {
                 }
             }
             .overlay {
-                if isHovering {
+                if isHovering && listElementText.isEmpty {
                     Button(action: {
                         isEditing = true
                         isHovering = false
@@ -39,14 +41,45 @@ struct ListElementView: View {
                             .cornerRadius(6)
                             .frame(width: 264, height: 44)
                             .overlay {
-                                if listElementText == "" {
-                                    Text("Add a new Task")
-                                        .foregroundStyle(.gray)
-                                        .font(.custom("Pretendard-Bold", size: 16))
-                                }
+                                Text("Add a new Task")
+                                    .foregroundStyle(.gray)
+                                    .font(.custom("Pretendard-Bold", size: 16))
                             }
                     }
                     .buttonStyle(PlainButtonStyle())
+                } else if isHovering && !listElementText.isEmpty {
+                    HStack(spacing: 0) {
+                        Button(action: {}) {
+                            Rectangle()
+                                .foregroundStyle(isLeftButtonOpening ? .red : .clear)
+                                .frame(width: 24, height: 48)
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                        .onHover { proxy in
+                            isLeftButtonOpening = proxy
+                        }
+                        Button(action: {
+                            isEditing = true
+                            isHovering = false
+                            isTextFieldFocused = true
+                            isEnterShorCut = false
+                        }) {
+                            Rectangle()
+                                .foregroundColor(.white.opacity(0.1))
+                                .frame(width: 216, height: 48)
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                        Button(action: {}) {
+                            Rectangle()
+                                .foregroundStyle(isRightButtonOpening ? .blue : .clear)
+                                .frame(width: 24, height: 48)
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                        .onHover { proxy in
+                            isRightButtonOpening = proxy
+                        }
+                    }
+                    .border(isHovering && !isLeftButtonOpening && !isRightButtonOpening ? .blue : .clear)
                 } else if isEditing {
                     TextField("", text: $listElementText, onCommit: {
                         isEditing = false
