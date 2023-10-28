b925e941d12ef0ba159ffed4cdf9d792f6806498
SeoYoung.An <102544840+abovocado@users.noreply.github.com>
2023-10-28 15:02:51 +0900

    [feat][#51] layerControlArea 스켈레톤

--- gridy/Sources/Presentations/PlanBoard/TimelineLayout/TimelineLayoutContentView.swift
+++ gridy/Sources/Presentations/PlanBoard/TimelineLayout/TimelineLayoutContentView.swift
@@ -11,26 +11,29 @@ import ComposableArchitecture
 struct TimelineLayoutContentView: View {
     @Namespace var scrollSpace
     @State var scrollOffset = CGFloat.zero
-    @Binding var showingIndexArea: Bool
+    // TODO: - 지우기, 아래 주석도
+//    @Binding var showingIndexArea: Bool
     @Binding var proxy: ScrollViewProxy?
     let store: StoreOf<PlanBoard>
     
     var body: some View {
         WithViewStore(store, observe: { $0 }){ viewStore in
             HStack(alignment: .top, spacing: 0) {
-                if showingIndexArea {
-                    VStack(spacing: 0) {
-                        ScheduleIndexAreaView()
-                            .frame(height: 140)
-                        Rectangle()
-                            .foregroundStyle(.white)
-                            .border(.black)
-                            .frame(height: 60)
-                        LineIndexAreaView()
-                    }
-                    .frame(width: 35)
-                    .zIndex(1)
+                //                if showingIndexArea {
+                VStack(spacing: 0) {
+                    ScheduleIndexAreaView()
+                        .frame(height: 140)
+                    Rectangle()
+                        .foregroundStyle(.white)
+                        .border(.black)
+                        .frame(height: 60)
+                    LineIndexAreaView()
                 }
+                .frame(width: 35)
+                .zIndex(1)
+                //                }
+                
+                
                 VStack(alignment: .leading, spacing: 0) {
                     BlackPinkInYourAreaView()
                         .frame(height: 200)
@@ -43,7 +46,7 @@ struct TimelineLayoutContentView: View {
                         ZStack(alignment: .bottom) {
                             ScheduleAreaView(store: store)
                                 .frame(height: 200)
-                
+                            
                             TimeAxisAreaView(store: store)
                                 .frame(height: 80)
                         }
@@ -52,6 +55,18 @@ struct TimelineLayoutContentView: View {
                         LineAreaView(store: store)
                             .zIndex(0)
                     }
+                }
+            }
+            
+            
+            VStack(spacing: 0) {
+                // MARK: - layerControlArea 상단
+                
+                // MARK: - layerControlArea
+                
+                // MARK: - layerControlArea 하단
+                HStack(alignment: .top, spacing: 0)  {
+                    
                 }
             }
         }
