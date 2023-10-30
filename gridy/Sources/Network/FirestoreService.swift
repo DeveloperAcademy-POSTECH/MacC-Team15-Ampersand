88a041cdeae8146c52b5bbdc597898f5537ab450
SeoYoung.An <102544840+abovocado@users.noreply.github.com>
2023-10-30 21:15:45 +0900

    [feat][#51] createPlan 위해 전반적 수정 FireStoreService 추가 API Service createPlan row 파라미터 추가 PlanBoard createPlan row 파라미터 추가 listAreaView index 조정 listElementView action createPlan row 파라미터 추가

---
+++ gridy/Sources/Network/FirestoreService.swift
@@ -0,0 +1,18 @@
+//
+//  FirestoreService.swift
+//  gridy
+//
+//  Created by SY AN on 10/30/23.
+//
+
+import SwiftUI
+
+struct FirestoreService: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FirestoreService()
+}
