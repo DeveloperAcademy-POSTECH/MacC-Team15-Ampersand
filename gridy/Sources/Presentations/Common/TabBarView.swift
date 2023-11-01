dc7198078f5c6d3a146036325e73f6d563db22ac
GaOn <xnoag@icloud.com>
2023-11-01 22:11:16 +0900

    [feat][#55] TabBarView 구조

--- gridy/Sources/Presentations/Common/TabBarView.swift
+++ gridy/Sources/Presentations/Common/TabBarView.swift
@@ -8,11 +8,84 @@
 import SwiftUI
 
 struct TabBarView: View {
+    @State var homeButtonHover: Bool = false
+    @State var homeButtonClicked: Bool = false
+    @State var planBoardTabHover: Bool = false
+    @State var planBoardTabClicked: Bool = false
+    @State var bellButtonHover: Bool = false
+    @Binding var bellButtonClicked: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(alignment: .center, spacing: 0) {
+            windowControlsButton
+            borderSpacer(.vertical)
+            homeButton
+            borderSpacer(.vertical)
+            planBoardTab
+            Spacer()
+            notificationButton
+        }
     }
 }
 
-#Preview {
-    TabBarView()
+extension TabBarView {
+    var windowControlsButton: some View {
+        HStack(alignment: .top, spacing: 8) {
+            Circle().foregroundStyle(.red).frame(width: 12, height: 12)
+            Circle().foregroundStyle(.yellow).frame(width: 12, height: 12)
+            Circle().foregroundStyle(.green).frame(width: 12, height: 12)
+        }
+        .padding(.horizontal, 12)
+    }
+}
+extension TabBarView {
+    var homeButton: some View {
+        Rectangle()
+            .foregroundStyle(homeButtonClicked ? .white : homeButtonHover ? .gray : .clear)
+            .overlay(
+                Image(systemName: "house.fill")
+                    .foregroundStyle(.black)
+            )
+            .frame(width: 36)
+            .onHover { proxy in
+                homeButtonHover = proxy
+            }
+            .onTapGesture { homeButtonClicked = true }
+    }
+}
+extension TabBarView {
+    var planBoardTab: some View {
+        HStack(alignment: .center, spacing: 0) {
+            Text("BoardNamed")
+                .padding(.leading, 16)
+                .foregroundStyle(planBoardTabClicked ? .black : planBoardTabHover ? .black : .white)
+            Rectangle()
+                .foregroundStyle(.clear)
+                .frame(width: 32)
+                .overlay(
+                    Image(systemName: "xmark").foregroundStyle(planBoardTabClicked ? .black : planBoardTabHover ? .black : .clear)
+                )
+        }
+        .background(planBoardTabClicked ? .white : planBoardTabHover ? .gray : .clear)
+        .onHover { proxy in
+            planBoardTabHover = proxy
+        }
+        .onTapGesture { planBoardTabClicked = true
+            print("ZZ")}
+    }
+}
+extension TabBarView {
+    var notificationButton: some View {
+        Rectangle()
+            .foregroundStyle(bellButtonClicked ? .white : bellButtonHover ? .gray : .clear)
+            .overlay(
+                Image(systemName: "bell.fill")
+                    .foregroundStyle(.black)
+            )
+            .frame(width: 48)
+            .onHover { proxy in
+                bellButtonHover = proxy
+            }
+            .onTapGesture { bellButtonClicked = true }
+    }
 }
--- gridy/Sources/Presentations/ProjectBoard/ProjectBoardView.swift
+++ gridy/Sources/Presentations/ProjectBoard/ProjectBoardView.swift
@@ -9,156 +9,55 @@ import SwiftUI
 import ComposableArchitecture
 
 struct ProjectBoardView: View {
-    let store: StoreOf<ProjectBoard>
-    @State var projectSearchText = ""
+    @State var bellButtonClicked = false
     
     var body: some View {
-        WithViewStore(store, observe: { $0 }) { viewStore in
-            GeometryReader { geometry in
-                ZStack {
-                    HStack(spacing: 0) {
-                        projectListArea
-                            .frame(width: 306)
-                        ProjectBoardMainView(store: store)
-                    }
-                    if viewStore.isCreationViewPresented {
-                        ZStack {
-                            Color.black.opacity(0.6)
-                                .frame(width: geometry.size.width, height: geometry.size.height)
-                                .onTapGesture {
-                                    viewStore.send(
-                                        ProjectBoard.Action.setSheet(isPresented: false)
-                                    )
-                                }
-                            ProjectCreationView(store: store)
-                                .offset(y: viewStore.isCreationViewPresented ? 0 : -50)
-                        }
-                        .onExitCommand {
-                            viewStore.send(
-                                .setSheet(isPresented: false)
-                            )
-                        }
-                    }
-                    
-                    if viewStore.isEditViewPresented {
-                        ZStack {
-                            Color.black.opacity(0.6)
-                                .frame(width: geometry.size.width, height: geometry.size.height)
-                                .onTapGesture {
-                                    viewStore.send(ProjectBoard.Action.setEditSheet(isPresented: false))
-                                }
-                            ProjectEditView(store: store)
-                                .offset(y: viewStore.isEditViewPresented ? 0 : -50)
-                        }
-                        .onExitCommand {
-                            viewStore.send(
-                                .setEditSheet(isPresented: false)
-                            )
-                        }
-                    }
+        VStack(alignment: .leading, spacing: 0) {
+            TabBarView(bellButtonClicked: $bellButtonClicked).frame(height: 36)
+            borderSpacer(.horizontal)
+            HStack(alignment: .top, spacing: 0) {
+                VStack(alignment: .leading, spacing: 0) {
+                    userSettingArea.frame(height: 60)
+                    borderSpacer(.horizontal)
+                    boardSearchArea.frame(height: 60)
+                    borderSpacer(.horizontal)
+                    projectListArea
                 }
+                .frame(width: 300)
+                borderSpacer(.vertical)
+                listArea
             }
         }
     }
 }
 
 extension ProjectBoardView {
-    
-    var projectListArea: some View {
-        
-        WithViewStore(store, observe: { $0 }) { viewStore in
-            GeometryReader { proxy in
-                VStack(alignment: .leading, spacing: 4) {
-                    HStack(alignment: .center, spacing: 4) {
-                        Circle()
-                            .foregroundStyle(.gray)
-                            .frame(width: 24, height: 24)
-                            .padding(6)
-                        Text("UserName")
-                            .font(.custom("Pretendard-SemiBold", size: 14))
-                            .multilineTextAlignment(.leading)
-                        Spacer()
-                    }
-                    .background(Color.gray.opacity(0.2))
-                    .frame(height: 36)
-                    
-                    RoundedRectangle(cornerRadius: 8)
-                        .foregroundStyle(.gray.opacity(0.1))
-                        .overlay {
-                            TextField("Search..", text: $projectSearchText)
-                                .textFieldStyle(.plain)
-                                .font(.custom("Pretendard-Regular", size: 14))
-                                .padding(.leading, 12)
-                        }
-                        .frame(height: 36)
-                        .padding(.horizontal, 12)
-                        .padding(.vertical)
-                    
-                    Button {
-                        // TODO: - New Folder Button
-                    } label: {
-                        RoundedRectangle(cornerRadius: 12)
-                            .foregroundStyle(.blue)
-                            .overlay {
-                                Text("New Folder")
-                                    .foregroundStyle(.white)
-                                    .font(.custom("Pretendard-Medium", size: 16))
-                            }
-                            .frame(height: 40)
-                            .padding(.vertical)
-                            .padding(.horizontal, 12)
-                    }
-                    HStack(alignment: .top, spacing: 4) {
-                        Spacer()
-                        Button {
-                            // TODO: - 왼쪽 버튼
-                            print("Button/1")
-                        } label: {
-                            RoundedRectangle(cornerRadius: 6)
-                                .foregroundStyle(.black.opacity(0.7))
-                                .frame(width: 24, height: 24)
-                        }
-                        Button {
-                            // TODO: - 오른쪽 버튼
-                            print("Button/2")
-                        } label: {
-                            RoundedRectangle(cornerRadius: 6)
-                                .foregroundStyle(.black.opacity(0.7))
-                                .frame(width: 24, height: 24)
-                        }
-                    }
-                    .padding(4)
-                    .background(Color.gray.opacity(0.5))
-                    HStack(spacing: 0) {
-                        Text("Directory")
-                            .font(.custom("Pretendard-Bold", size: 18))
-                            .padding()
-                        Spacer()
-                    }
-                    List {
-                        ForEachStore(
-                            store.scope(
-                                state: \.projects,
-                                action: { .deleteProjectButtonTapped(id: $0, action: $1) }
-                            )
-                        ) {
-                            ProjectSideItemView(store: $0)
-                                .listRowSeparator(.hidden)
-                                .listRowInsets(EdgeInsets(top: 0, leading: -16, bottom: 0, trailing: 0))
-                                .frame(width: proxy.size.width, height: 32)
-                        }
-                    }
-                }
-                .background(.white)
-                .buttonStyle(PlainButtonStyle())
-            }
-            .frame(width: 306)
-            .onAppear {
-                viewStore.send(
-                    .onAppear
-                )
-            }
-        }
+    var userSettingArea: some View {
+        Text("userSettingArea")
     }
 }
 
+extension ProjectBoardView {
+    var boardSearchArea: some View {
+        Text("boardSearchArea")
+    }
+}
+
+extension ProjectBoardView {
+    var projectListArea: some View {
+        Text("projectListArea")
+    }
+}
+
+extension ProjectBoardView {
+    var listArea: some View {
+        Text("listArea")
+    }
+}
+
+func borderSpacer(_ direction: Edge.Set) -> some View {
+    Rectangle()
+        .foregroundStyle(.black)
+        .frame(width: direction == .vertical ? 1 : .infinity)
+        .frame(height: direction == .horizontal ? 1 : .infinity)
+}
