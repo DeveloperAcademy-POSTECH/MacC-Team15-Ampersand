e86cd097a2cbc2b95452257053fdb8db549f1e0f
GaOn <xnoag@icloud.com>
2023-11-04 22:26:47 +0900

    [feat][#62] NotificationView

--- gridy/Sources/Presentations/Common/NotificationView.swift
+++ gridy/Sources/Presentations/Common/NotificationView.swift
@@ -8,25 +8,68 @@
 import SwiftUI
 
 struct NotificationView: View {
-    @Environment(\.dismiss) private var dismiss
-    
     var body: some View {
-        NavigationStack {
-            Text("NotificationView")
-                .scenePadding()
+        VStack(alignment: .leading, spacing: 8) {
+            titleAndFeedbackArea
+            notifications
         }
-        .toolbar {
-            ToolbarItem(placement: .cancellationAction) {
-                Button("Cancel") {
-                    dismiss()
-                }
-            }
-            ToolbarItem(placement: .confirmationAction) {
-                Button("Continue") {
-                    dismiss()
-                }
-            }
-        }
-        .frame(width: 600, height: 350)
+        .frame(width: 275, height: 424)
     }
+}
+
+extension NotificationView {
+    var titleAndFeedbackArea: some View {
+        HStack(alignment: .center) {
+            Text("Notifications")
+                .font(.title2)
+                .fontWeight(.bold)
+                .foregroundStyle(Color.title)
+            Spacer()
+            Button {
+                // TODO: - Feedback Button
+            } label: {
+                Text("􀁞 Feedback")
+                    .font(.caption)
+                    .fontWeight(.medium)
+                    .foregroundStyle(.blue)
+            }
+            .buttonStyle(.plain)
+        }
+        .padding(16)
+        .background(Color.blackWhite)
+    }
+}
+
+extension NotificationView {
+    var notifications: some View {
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(alignment: .center, spacing: 8) {
+                ForEach(0..<5) { index in
+                    Notifications(id: index)
+                }
+            }
+        }
+        .padding(.horizontal, 16)
+    }
+    
+    private struct Notifications: View {
+        var id: Int
+        
+        var body: some View {
+            VStack(alignment: .leading, spacing: 0) {
+                Text("Notification Content \(id)")
+                    .font(.headline)
+                    .fontWeight(.medium)
+                    .foregroundStyle(Color.title)
+                Text("Date")
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .foregroundStyle(Color.subtitle)
+            }
+        }
+    }
+}
+
+#Preview {
+    NotificationView()
 }
