0d05f2f7872671291b0341998c079287d1a2cf8f
Jin-s-work <9114jin@hanmail.net>
2023-11-05 00:53:42 +0900

    [feat][#63] ShareImageView 뷰 만들기

--- gridy/Sources/Presentations/PlanBoard/ShareImageView.swift
+++ gridy/Sources/Presentations/PlanBoard/ShareImageView.swift
@@ -2,32 +2,150 @@
 //  ShareImageView.swift
 //  gridy
 //
-//  Created by xnoag on 11/1/23.
+//  Created by Royce on 11/1/23.
 //
 
 import SwiftUI
 
 struct ShareImageView: View {
     @Environment(\.dismiss) private var dismiss
+    @State private var allPeriodsChecked = false
+    @State private var startDateChecked = false
+    @State private var cancelHovered = false
+    @State private var isStartDateHovered = false
+    @State private var isEndDateHovered = false
     
     var body: some View {
-        NavigationStack {
-            Text("ShareImageView")
-                .scenePadding()
-        }
-        .toolbar {
-            ToolbarItem(placement: .cancellationAction) {
-                Button("Cancel") {
-                    dismiss()
+        VStack(spacing : 16){
+            RoundedRectangle(cornerRadius: 8)
+                .overlay(
+                    Text("Thumbnail")
+                        .foregroundStyle(Color.subtitle) /// Color #747474로 바꿔야함
+                )
+                .foregroundStyle(Color.item)
+                .frame(width:436, height:330)
+            RoundedRectangle(cornerRadius: 8)
+                .overlay(){
+                    VStack(alignment:.leading) {
+                        HStack(spacing : 8) {
+                            RadioButton(isSelected: $allPeriodsChecked)
+                                .padding(.leading, 16)
+                            Text("기간 모두")
+                                .foregroundColor(Color.title)
+                        }
+                        HStack() {
+                            RadioButton(isSelected: $startDateChecked)
+                                .padding(.leading, 16)
+                            Text("기간 설정")
+                                .foregroundColor(Color.title)
+                            Spacer()
+                                .frame(width:16)
+                            
+                            Text("시작 : 2023.10.05.")
+                                .foregroundColor(Color.title)
+                                .frame(width: 132, height: 32)
+                                .background(isStartDateHovered ? Color.itemHovered : Color.clear)
+                                .cornerRadius(5)
+                                .onHover { hover in
+                                    isStartDateHovered = hover
+                                }
+                            Spacer()
+                                .frame(width:8)
+
+                            Text("종료 : 2023.11.25.")
+                                .foregroundColor(Color.title)
+                                .frame(width: 132, height: 32)
+                                .background(isEndDateHovered ? Color.itemHovered : Color.clear)
+                                .cornerRadius(5)
+                                .onHover { hover in
+                                    isEndDateHovered = hover
+                                }
+                            Spacer()
+                        }
+                    }
                 }
-            }
-            ToolbarItem(placement: .confirmationAction) {
-                Button("Continue") {
+                .foregroundStyle(Color.item)
+                .frame(width:436, height:88)
+            
+            borderSpacer(.horizontal)
+                .padding(.horizontal, 16)
+
+            HStack(){
+                Button {
                     dismiss()
+                } label: {
+                    RoundedRectangle(cornerRadius: 5)
+                        .foregroundStyle(Color.item)
+                        .frame(width: 80, height: 24)
+                        .overlay(
+                            Text("Share")
+                                .font(.system(size: 13))
+                                .foregroundStyle(Color.button)
+                        )
                 }
+                .buttonStyle(.link)
+                Spacer()
+                Button {
+                    dismiss()
+                } label: {
+                    RoundedRectangle(cornerRadius: 5)
+                        .foregroundStyle(cancelHovered ? Color.itemHovered : Color.item)
+                        .frame(width: 80, height: 24)
+                        .overlay(
+                            Text("Cancel")
+                                .font(.system(size: 13))
+                                .foregroundStyle(Color.button)
+                        )
+                }
+                .buttonStyle(.link)
+                .onHover { hover in
+                    cancelHovered = hover
+                }
+                Button {
+                    dismiss()
+                } label: {
+                    RoundedRectangle(cornerRadius: 5)
+                        .foregroundStyle(Color.item)
+                        .frame(width: 80, height: 24)
+                        .overlay(
+                            Text("Create")
+                                .font(.system(size: 13))
+                                .foregroundStyle(Color.button)
+                        )
+                }
+                .buttonStyle(.link)
+            }
+            .padding(.horizontal, 16)
+        }
+        .frame(width: 468, height: 523)
+        .padding(16)
+        .background(Color.gray.opacity(0.3))
+        
+    }
+}
+
+/// radio button custom
+struct RadioButton: View {
+    @Binding var isSelected: Bool
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(Color.white)
+                .frame(width: 14, height: 14)
+            
+            if isSelected {
+                Circle()
+                    .fill(Color.blue)
+                    .frame(width: 14, height: 14)
+                Circle()
+                    .fill(Color.white)
+                    .frame(width: 6, height: 6)
             }
         }
-        .frame(width: 600, height: 350)
+        .onTapGesture {
+            isSelected.toggle()
+        }
     }
 }
 
