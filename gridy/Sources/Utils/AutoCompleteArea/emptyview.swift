d3baf928206293347a057ffe4a46bcb2de6c55be
Jin-s-work <9114jin@hanmail.net>
2023-11-05 15:27:05 +0900

    [fix][#48] 새로운 파일로 push 확인하기

---
+++ gridy/Sources/Utils/AutoCompleteArea/emptyview.swift
@@ -0,0 +1,18 @@
+//
+//  emptyview.swift
+//  gridy
+//
+//  Created by Jin Sang woo on 11/5/23.
+//
+
+import SwiftUI
+
+struct emptyview: View {
+    var body: some View {
+        Text("This is for test")
+    }
+}
+
+#Preview {
+    emptyview()
+}
