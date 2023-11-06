ee9d711b2ea05c1c39cf2f0528f0715e3cffdf07
Jin-s-work <9114jin@hanmail.net>
2023-11-06 21:13:28 +0900

    [fix][#63] TimelineLayoutView에 있는 추가적인 코드 설명 추가

--- gridy/Sources/Presentations/PlanBoard/TimelineLayoutView.swift
+++ gridy/Sources/Presentations/PlanBoard/TimelineLayoutView.swift
@@ -54,6 +54,7 @@ struct TimelineLayoutView: View {
                 lineArea
             }
         }
+        /// 실제로는 지정되지 않지만 기능 확인하는 용인 frame
         .frame(width: 1511, height: 934)
         .sheet(isPresented: $bellButtonClicked) {
             NotificationView()
