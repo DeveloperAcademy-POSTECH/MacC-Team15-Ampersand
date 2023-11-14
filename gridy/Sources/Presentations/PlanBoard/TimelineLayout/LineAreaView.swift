30b57ad09ffcc45f48f6a4000f414b1d195d2533
Jin-s-work <9114jin@hanmail.net>
2023-11-14 17:06:07 +0900

    [feat][#75] 드래그를 끝내고 새로 드래그 해도 스케줄라인이 만들어지게 하기

--- gridy/Sources/Presentations/PlanBoard/TimelineLayout/LineAreaView.swift
+++ gridy/Sources/Presentations/PlanBoard/TimelineLayout/LineAreaView.swift
@@ -12,6 +12,8 @@ struct DrawnLine: Identifiable, Equatable {
     let id = UUID()
     var start: CGPoint
     var end: CGPoint
+    var diamondStart: CGPoint
+    var diamondEnd: CGPoint
 }
 
 struct LineAreaView: View {
@@ -27,7 +29,6 @@ struct LineAreaView: View {
     @State private var lineEnd: CGPoint = .zero
     @State private var diamondPosition: CGPoint = .zero
     @State private var drawnLines: [DrawnLine] = []
-    
     
     let store: StoreOf<PlanBoard>
     
@@ -205,71 +206,56 @@ struct LineAreaView: View {
                         let startX = CGFloat(range.start.col) * viewStore.gridWidth
                         let endX = CGFloat(range.end.col + 1) * viewStore.gridWidth
                         
-                        let isHorizontalDrag = abs(endX - startX) > abs(endY - startY)
+                        Rectangle()
+                            .fill(Color.black)
+                            .frame(width: abs(endX - startX), height: 2)
+                            .position(x: (startX + endX) / 2, y: startY)
                         
-                        if isHorizontalDrag {
-                            Rectangle()
-                                .fill(Color.black)
-                                .frame(width: abs(endX - startX), height: 2)
-                                .position(x: (startX + endX) / 2, y: startY) // 수평선을 드래그 시작점과 끝점의 중간점에 위치
+                        let startcenterX = startX
+                        let centerX = endX
+                        let centerY = startY
+                        
+                        Path { path in
+                            let diamondSize: CGFloat = 10
                             
-                            let startcenterX = startX
-                            let centerX = endX
-                            let centerY = startY
+                            path.move(to: CGPoint(x: startcenterX, y: centerY - diamondSize / 2))
+                            path.addLine(to: CGPoint(x: startcenterX + diamondSize / 2, y: centerY))
+                            path.addLine(to: CGPoint(x: startcenterX, y: centerY + diamondSize / 2))
+                            path.addLine(to: CGPoint(x: startcenterX - diamondSize / 2, y: centerY))
                             
-                            Path { path in
-                                let diamondSize: CGFloat = 10
-                                
-                                path.move(to: CGPoint(x: startcenterX, y: centerY - diamondSize / 2))
-                                path.addLine(to: CGPoint(x: startcenterX + diamondSize / 2, y: centerY))
-                                path.addLine(to: CGPoint(x: startcenterX, y: centerY + diamondSize / 2))
-                                path.addLine(to: CGPoint(x: startcenterX - diamondSize / 2, y: centerY))
-                                
-                                path.move(to: CGPoint(x: centerX, y: centerY - diamondSize / 2))
-                                path.addLine(to: CGPoint(x: centerX + diamondSize / 2, y: centerY))
-                                path.addLine(to: CGPoint(x: centerX, y: centerY + diamondSize / 2))
-                                path.addLine(to: CGPoint(x: centerX - diamondSize / 2, y: centerY))
-                                path.closeSubpath()
-                                
-                            }
-                            .fill(Color.purple)
-                        } else {
-                            let lineX = endX
-                            Rectangle()
-                                .fill(Color.black)
-                                .frame(width: 2, height: abs(endY - startY))
-                                .position(x: lineX, y: (startY + endY) / 2)
+                            path.move(to: CGPoint(x: centerX, y: centerY - diamondSize / 2))
+                            path.addLine(to: CGPoint(x: centerX + diamondSize / 2, y: centerY))
+                            path.addLine(to: CGPoint(x: centerX, y: centerY + diamondSize / 2))
+                            path.addLine(to: CGPoint(x: centerX - diamondSize / 2, y: centerY))
+                            path.closeSubpath()
                             
-                            let diamondX = lineX
-                            let diamondY = endY
-                            let startdiamondY = startY
-                            
-                            Path { path in
-                                let diamondSize: CGFloat = 10
-                                path.move(to: CGPoint(x: diamondX, y: startdiamondY - diamondSize / 2))
-                                path.addLine(to: CGPoint(x: diamondX + diamondSize / 2, y: startdiamondY))
-                                path.addLine(to: CGPoint(x: diamondX, y: startdiamondY + diamondSize / 2))
-                                path.addLine(to: CGPoint(x: diamondX - diamondSize / 2, y: startdiamondY))
-                                
-                                path.move(to: CGPoint(x: diamondX, y: diamondY - diamondSize / 2))
-                                path.addLine(to: CGPoint(x: diamondX + diamondSize / 2, y: diamondY))
-                                path.addLine(to: CGPoint(x: diamondX, y: diamondY + diamondSize / 2))
-                                path.addLine(to: CGPoint(x: diamondX - diamondSize / 2, y: diamondY))
-                                path.closeSubpath()
-                            }
-                            .fill(Color.purple)
                         }
+                        .fill(Color.purple)
+                        
                     }
                     
-                    //                    ForEach(viewStore.drawnLines) { line in
-                    //
-                    //                        Path { path in
-                    //                            path.move(to: line.start)
-                    //                            path.addLine(to: line.end)
-                    //
-                    //                        }
-                    //                        .stroke(Color.black, lineWidth: 2)
-                    //                    }
+                    ForEach(drawnLines) { line in
+                        Path { path in
+                            path.move(to: line.start)
+                            path.addLine(to: line.end)
+                        }
+                        .stroke(Color.black, lineWidth: 2)
+                        
+                        Path { path in
+                            let diamondSize: CGFloat = 10
+                            path.move(to: CGPoint(x: line.start.x, y: line.start.y - diamondSize / 2))
+                            path.addLine(to: CGPoint(x: line.start.x + diamondSize / 2, y: line.start.y))
+                            path.addLine(to: CGPoint(x: line.start.x, y: line.start.y + diamondSize / 2))
+                            path.addLine(to: CGPoint(x: line.start.x - diamondSize / 2, y: line.start.y))
+                            
+                            path.move(to: CGPoint(x: line.end.x, y: line.end.y - diamondSize / 2))
+                            path.addLine(to: CGPoint(x: line.end.x + diamondSize / 2, y: line.end.y))
+                            path.addLine(to: CGPoint(x: line.end.x, y: line.end.y + diamondSize / 2))
+                            path.addLine(to: CGPoint(x: line.end.x - diamondSize / 2, y: line.end.y))
+                        }
+                        .fill(Color.red)
+                    }
+                    
                 }
                 
                 .frame(width: geometry.size.width, height: geometry.size.height)
@@ -342,39 +328,17 @@ struct LineAreaView: View {
                             let endRow = Int(dragEnd.y / viewStore.lineAreaGridHeight)
                             let endCol = Int(dragEnd.x / viewStore.gridWidth)
                             /// 드래그 해서 화면 밖으로 나갔는지 Bool로 반환 (Left, Right, Top, Bottom)
-                            ///
-                            // 드래그 방향 판단 (수직 드래그인 경우)
-                            let isVerticalDrag = abs(dragEnd.y - dragStart.y) > abs(dragEnd.x - dragStart.x)
                             
-                            if isVerticalDrag {
-                                let startY = min(dragStart.y, dragEnd.y)
-                                let endY = max(dragStart.y, dragEnd.y)
-                                let colX = dragEnd.x // 혹은 dragStart.x
-                                
-                                // 선의 끝점 계산 (오른쪽 끝)
-                                let lineEndX = colX + viewStore.gridWidth
-                                
-                                // 다이아몬드의 위치 결정
-                                let diamondY = (startY + endY) / 2 // 세로 방향 중앙
-                                let diamondX = lineEndX + 10 // 선의 끝에서 오른쪽으로 10pt
-                                
-                                // 상태 변수 업데이트
-                                self.drawLine = true
-                                self.lineStart = CGPoint(x: colX, y: startY)
-                                self.lineEnd = CGPoint(x: lineEndX, y: endY)
-                                self.diamondPosition = CGPoint(x: diamondX, y: diamondY)
-                            }
-                            else{
-                                let startX = min(dragStart.x, dragEnd.x)
-                                let endX = max(dragStart.x, dragEnd.x)
-                                let rowY = dragEnd.y // 혹은 dragStart.y
-                                
-                                // 상태 변수 업데이트
-                                self.drawLine = true
-                                self.lineStart = CGPoint(x: startX, y: rowY)
-                                self.lineEnd = CGPoint(x: endX, y: rowY)
-                                self.diamondPosition = CGPoint(x: (startX + endX) / 2, y: rowY - 10)
-                            }
+                            let startX = min(dragStart.x, dragEnd.x)
+                            let endX = max(dragStart.x, dragEnd.x)
+                            let rowY = dragEnd.y // 혹은 dragStart.y
+                            
+                            // 상태 변수 업데이트
+                            self.drawLine = true
+                            self.lineStart = CGPoint(x: startX, y: rowY)
+                            self.lineEnd = CGPoint(x: endX, y: rowY)
+                            self.diamondPosition = CGPoint(x: (startX + endX) / 2, y: rowY - 10)
+                            
                             self.drawLine = true
                             
                             exceededDirection = [
@@ -420,7 +384,6 @@ struct LineAreaView: View {
                                 }
                             }
                         }
-                    
                         .onEnded { gesture in
                             viewStore.send(.dragGestureEnded(temporarySelectedGridRange))
                             
@@ -430,7 +393,26 @@ struct LineAreaView: View {
                                 temporarySelectedGridRange = nil
                             }
                             
-                            let newLine = DrawnLine(start: self.lineStart, end: self.lineEnd)
+                            let dragStart = gesture.startLocation
+                            let dragEnd = gesture.location
+                            let startRow = Int(dragStart.y / viewStore.lineAreaGridHeight)
+                            let endRow = Int(dragEnd.y / viewStore.lineAreaGridHeight)
+                            let startCol = Int(dragStart.x / viewStore.gridWidth)
+                            let endCol = Int(dragEnd.x / viewStore.gridWidth)
+                            
+                            let rowBottomY = CGFloat(startRow + 1) * viewStore.lineAreaGridHeight
+                            let colLeftX = CGFloat(min(startCol, endCol)) * viewStore.gridWidth
+                            let colRightX = CGFloat(max(startCol, endCol) + 1) * viewStore.gridWidth
+                            
+                            self.lineStart = CGPoint(x: colLeftX, y: rowBottomY)
+                            self.lineEnd = CGPoint(x: colRightX, y: rowBottomY)
+                            
+                            let diamondStart = CGPoint(x: colLeftX, y: self.lineStart.y)
+                            let diamondEnd = CGPoint(x: colRightX, y: self.lineEnd.y)
+                            
+                            let newLine = DrawnLine(start: self.lineStart, end: self.lineEnd, diamondStart: diamondStart, diamondEnd: diamondEnd)
+                            self.drawnLines.append(newLine)
+                            
                             temporarySelectedGridRange = nil
                             exceededDirection = [false, false, false, false]
                         }
@@ -441,8 +423,6 @@ struct LineAreaView: View {
                             viewStore.send(.magnificationChangedInListArea(value, geometry.size))
                         }
                 )
-                
-                
             }
         }
     }
